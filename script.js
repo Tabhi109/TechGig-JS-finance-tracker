dcb89bc37c9b2ffe5ccc7e68cdda11e07887bca5
Tabhi109 <tabhinav109@gmail.com>
2023-05-30 22:41:39 +0530

    Created an expense tracker in js!

---
+++ script.js
@@ -0,0 +1,38 @@
+const transAmt = document.getElementById('amount');
+const transDesc = document.getElementById('transac');
+const transBtn = document.getElementById("addbtn");
+const incomeCheck = document.getElementById("income-check");
+const expenseCheck = document.getElementById("expense-check");
+const bal = document.getElementById('bal');
+const inc = document.getElementById('inc');
+const exp = document.getElementById('exp');
+const incomeList = document.getElementById('income-list');
+const expenseList = document.getElementById('expense-list');
+
+let income = 0;
+let expense = 0;
+let balance = 0;
+
+transBtn.addEventListener('click', () => {
+    var isIncSelected = incomeCheck.checked;
+    var isExpSelected = expenseCheck.checked;
+    const amount = Number(transAmt.value);
+    if (isIncSelected) {
+        income += amount;
+        balance += amount;
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `${transDesc.value} <span class="transaction-amount">(+${amount})</span>`;
+        incomeList.appendChild(listItem);
+    } else if (isExpSelected) {
+        expense += amount;
+        balance -= amount;
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `${transDesc.value} <span class="transaction-amount">(-${amount})</span>`;
+        expenseList.appendChild(listItem);
+    } else {
+        console.log('Error');
+    }
+    bal.innerHTML = balance;
+    inc.innerHTML = income;
+    exp.innerHTML = expense;
+});
